bc62a22cc7246b0020f15455f1389e51fe9754f3
unknown <cogjava4347@iiht.tech>
2022-11-10 18:27:05 +0530

    Caluclator

---
+++ JUnit5DemoProject/src/com/junit/test/Caluclator.java
@@ -0,0 +1,20 @@
+package com.junit.test;
+
+public class Caluclator {
+	public static void main(String[] args) {
+	}
+		public int add(int num1, int num2) {
+			int total = num1 + num2;
+			return total;
+		}
+		public int sub(int num1, int num2) {
+			int total = num1 - num2;
+			return total;
+		}
+		public int product(int num1, int num2) {
+			int total =  num1*num2;
+			return total;
+		}
+	}
+
+
