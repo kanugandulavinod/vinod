5456145147b1a1a341956d5b0134dc5ee41d2a35
unknown <cogjava4347@iiht.tech>
2022-11-11 11:17:16 +0530

    TV1

--- OOP-basic/src/com/oop/TV.java
+++ OOP-basic/src/com/oop/TV.java
@@ -1,5 +1,24 @@
 package com.oop;
 
 public class TV {
+String Brand;
+String ModelName;
+Integer screenSize;
+Integer price;
+String colour;
+String DisplayType;
+
+public static void main(String[] args) {
+	 TV lgOled = new TV();
+	 TV SamsungFrame = new TV();
+	 lgOled.Brand = "LG";
+	 lgOled.ModelName = "4k oled 75 tv";
+	 lgOled.screenSize = 75;
+	 lgOled.price = 100000;
+	 lgOled.colour = "black";
+	 lgOled.DisplayType = "OLED";
+	 System.out.println(lgOled.Brand + " " + lgOled.ModelName );
+			 
+}
 
 }
